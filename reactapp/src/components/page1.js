4424867055474770654024c070fcc365897dc99c
Srijamadigithub <110340477+Srijamadigithub@users.noreply.github.com>
2022-08-05 18:34:12 +0530

    assignent

---
+++ reactapp/src/components/page1.js
@@ -0,0 +1,11 @@
+import React from"react"
+import'./style.css';
+function page1()
+{
+    return (
+        <div>
+            <h1>This is page 1</h1>
+        </div>
+    )
+}
+export default page1
